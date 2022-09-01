1fc7be73de9a03cadf4b96a4dc85d1b3e5f81450
Egor <94043508+SVENmods@users.noreply.github.com>
2022-09-01 22:50:10 +0300

    end main content

--- js/script.js
+++ js/script.js
@@ -1,47 +1,45 @@
 $(document).ready(function () {
-     // tab content
-     $(".main-container__content").fadeOut();
-     $(".main-container__content:first").fadeIn();
-      // if in tab mode
-     $("ul.tabs li a").click(function() {
-          $(".main-container__content").hide();
-          var activeTab = $(this).attr("href"); 
-          $(activeTab).fadeIn();    
-          $("ul.tabs li a").removeClass("active");
-          $(this).addClass("active");
-     });
+  $(".main-container__content").fadeOut();
+  $(".main-container__content:first").fadeIn();
+  $("ul.tabs li a").click(function() {
+    $(".main-container__content").hide();
+    var activeTab = $(this).attr("href"); 
+    $(activeTab).fadeIn();    
+    $("ul.tabs li a").removeClass("active");
+    $(this).addClass("active");
+  });
 });
 
 function autoType(elementClass, typingSpeed){
-     var thhis = $(elementClass);
-     thhis.css({
-       "position": "relative",
-       "display": "inline-block"
-     });
-     thhis.prepend('<div class="cursor-text" style="right: initial; left:0;"></div>');
-     thhis = thhis.find(".text-js");
-     var text = thhis.text().trim().split('');
-     var amntOfChars = text.length;
-     var newString = "";
-     thhis.text("|");
-     setTimeout(function(){
-       thhis.css("opacity",1);
-       thhis.prev().removeAttr("style");
-       thhis.text("");
-       for(var i = 0; i < amntOfChars; i++){
-         (function(i,char){
-           setTimeout(function() {        
-             newString += char;
-             thhis.text(newString);
-           },i*typingSpeed);
-         })(i+1,text[i]);
-       }
-     },1500);
-   }
-   
-   $(document).ready(function(){
-     // Now to start autoTyping just call the autoType function with the 
-     // class of outer div
-     // The second paramter is the speed between each letter is typed.   
-     autoType(".type-js",200);
-   });
+  var thhis = $(elementClass);
+  thhis.css({
+    "position": "relative",
+    "display": "inline-block"
+  });
+  thhis.prepend('<div class="cursor-text" style="right: initial; left:0;"></div>');
+  thhis = thhis.find(".text-js");
+  var text = thhis.text().trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  thhis.text("|");
+  setTimeout(function(){
+    thhis.css("opacity",1);
+    thhis.prev().removeAttr("style");
+    thhis.text("");
+    for(var i = 0; i < amntOfChars; i++){
+      (function(i,char){
+        setTimeout(function() {        
+          newString += char;
+          thhis.text(newString);
+        },i*typingSpeed);
+      })(i+1,text[i]);
+      }
+  },1500);
+};
+
+$(document).ready(function(){
+    autoType(".type-js",200);
+    setTimeout(function(){
+      $(".main-container__main-text").fadeIn("slow");
+    },6000);
+});
